b6b1de787995d3dac466ec4ad603d3f87c9ce6be
Pravin Khillare <153601072+pravin-khillare@users.noreply.github.com>
2025-05-15 13:40:35 +0530

    Update and rename main.cpp to ColorCoder.cpp

---
+++ ColorCoder.cpp
@@ -0,0 +1,43 @@
+// ColorCoder.cpp
+#include "ColorCoder.h"
+#include <stdexcept>
+
+namespace TelCoColorCoder {
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+
+    ColorPair::ColorPair(MajorColor major, MinorColor minor)
+        : majorColor(major), minorColor(minor) {}
+
+    MajorColor ColorPair::getMajor() {
+        return majorColor;
+    }
+
+    MinorColor ColorPair::getMinor() {
+        return minorColor;
+    }
+
+    std::string ColorPair::ToString() {
+        std::string colorPairStr = MajorColorNames[majorColor];
+        colorPairStr += " ";
+        colorPairStr += MinorColorNames[minorColor];
+        return colorPairStr;
+    }
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = static_cast<MajorColor>(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = static_cast<MinorColor>(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
